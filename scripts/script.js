4f35623d720373da7f9a921b9711ed52c6ea0b5a
Eli <e.narvydas@outlook.com>
2020-11-18 04:38:16 +0000

    Fixed button css to correctly overlay over library entries

--- scripts/script.js
+++ scripts/script.js
@@ -34,10 +34,12 @@ function displayBooks() {
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    document.querySelector(".open-button").style.display = "none";
 
 }
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
+    document.querySelector(".open-button").style.display = "block";
     let form = document.getElementById("myForm");
     console.log(typeof form);
     resetForm(form);
